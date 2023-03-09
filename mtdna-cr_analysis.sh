f51dcd3aae190bde00794e35f81ae9fb51118e0e
Daniel Ruiz de la Cuesta Aguirre <97441691+DanielRCA@users.noreply.github.com>
2023-03-09 15:17:52 +0100

    Update mtdna-cr_analysis.sh

--- mtdna-cr_analysis.sh
+++ mtdna-cr_analysis.sh
@@ -2,7 +2,19 @@
 ##${PMDTOOLS_ROUTE} is the route where pmdtools.0.60.py (PMDtools script) is saved
 
 #Sequencing quality control
-fastqc -f fastq *fq.gz
+fastqc -f fastq *fastq.gz
+
+#Duplicates are removed with fastp
+fastp --verbose \
+        --dedup \
+        --disable_length_filtering \
+        --disable_quality_filtering \
+        --json ${name}_dedup_temp.json \
+        --thread 8 \
+        --out1 ${name}_fastp_dedup_R1_temp.fq.gz \
+        --out2 ${name}_fastp_dedup_R2_temp.fq.gz \
+        --in1 *R1*.fastq.gz \
+        --in2 *R2*.fastq.gz
 
 #Adapters are trimmed with fastp
 fastp --verbose \
@@ -16,8 +28,8 @@ fastp --verbose \
   --thread 8 \
   --out1 ${name}_fastp_filtered_R1.fq.gz \
   --out2 ${name}_fastp_filtered_R2.fq.gz \
-  --in1 *R1*.fastq*.gz \
-  --in2 *R2*.fastq*.gz
+  --in1 ${name}_fastp_dedup_R1_temp.fq.gz \
+  --in2 ${name}_fastp_dedup_R2_temp.fq.gz 
        
 #25 bp (primers) are trimmed with another fastp
 fastp --verbose \
@@ -26,21 +38,40 @@ fastp --verbose \
   --json ${name}_fastp.json \
   --html ${name}_fastp.html \
   --report_title ${name} \
-  --out1 ${name}_R1.fq.gz \
-  --out2 ${name}_R2.fq.gz \
+  --out1 ${name}_nonprimer_R1_temp.fq.gz \
+  --out2 ${name}_nonprimer_R2_temp.fq.gz \
   --in1 ${name}_fastp_filtered_R1.fq.gz \
   --in2 ${name}_fastp_filtered_R2.fq.gz
 
-#Both final trimmed fastQ files are aligned
-bwa aln reference_mt.fa ${name}_R1.fq.gz > aln_R1.sai
-bwa aln reference_mt.fa ${name}_R2.fq.gz > aln_R2.sai
-bwa sampe reference_mt.fa aln_R1.sai aln_R2.sai ${name}_R1.fq.gz ${name}_R2.fq.gz | samtools view -Sbho ${name}_aln.bam
+#Paired End FastQ files are merged
+fastp --verbose \
+      --cut_tail --cut_tail_mean_quality 20 \
+      --trim_poly_x \
+      --merge --overlap_len_require 10 \
+      --correction \
+      --html ${name}_temp.html \
+      --json ${name}_temp.json \
+      --merged_out ${d}_M.fq.gz \
+      --out1 ${name}_R1.fq.gz \
+      --out2 ${name}_R2.fq.gz \
+      --in1 ${name}_nonprimer_R1_temp.fq.gz \
+      --in2 ${name}_nonprimer_R2_temp.fq.gz
 
-#Reds that did not aligned with the reference genome with a quality higher than 30 are discarded
-samtools view -bh -F4 -q 30 ${name}_aln.bam > ${name}_map.bam
+#Trimmed FastQ files quality control
+fastqc -f fastq *.fq.gz
+
+#Trimmed FastQ files are aligned with the refernece genome and only those with a quality higher than 30 are kept
+bwa aln -l 1024 -n 0.01 -o 2 reference_mt.fa ${name}_R1.fq.gz > aln_R1.sai
+bwa aln -l 1024 -n 0.01 -o 2 reference_mt.fa ${name}_R2.fq.gz > aln_R2.sai
+bwa aln -l 1024 -n 0.01 -o 2 reference_mt.fa ${name}_M.fq.gz > aln_M.sai
+bwa sampe reference_mt.fa aln_R1.sai aln_R2.sai ${name}_R1.fq.gz ${name}_R2.fq.gz | samtools view -Sbh -F4 -q30 | samtools sort -O bam -o tem_${name}_NM_map.bam
+bwa samse reference_mt.fa aln_M.sai ${name}_M.fq.gz | samtools view -Sbh -F4 -q30 | samtools sort -O bam -o tem_${name}_M_map.bam
+
+#Aligned bam files (merged and non-merged Paired End reads) are merged
+samtools merge -o tem_${name}_map_unsort.bam tem_${name}_NM_map.bam tem_${name}_M_map.bam
 
 #BAM file is sorted
-samtools sort -O bam -o ${name}.bam ${name}_map.bam
+samtools sort -O bam -o ${name}.bam tem_${name}_map_unsort.bam
 
 #BAM file is indexed
 samtools index ${name}.bam
@@ -49,7 +80,7 @@ samtools index ${name}.bam
 qualimap bamqc -bam ${name}.bam -c -gd hg19 -outdir ${name}_qualimap_bamqc  
 
 #Reads with Post-Mortem Damage are selected
-samtools view -h ${name}.bam | python ${PMDTOOLS_ROUTE}/pmdtools.0.60.py--threshold 0 --header | samtools view -Sb - > pmd_${name}.bam
+samtools view -h ${name}.bam | python ${PMDTOOLS_ROUTE}/pmdtools.0.60.py--threshold 1 --header | samtools view -Sb - > pmd_${name}.bam
 
 #Polimorphisms are obteined with freebayes
-freebayes -f reference_mt.fa -m 20 -q 30 -i -X -u -F 0.3 -C 10 ${name}.bam > ${name}.vcf 
+freebayes -f reference_mt.fa -i -X -F 0.1 -C 3 --min-coverage 30 ${name}.bam | vcfallelicprimitives -kg > ${name}.vcf
